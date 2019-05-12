ecfe68d10ed2a051f1ff57aed719b4cb8247dbf3
joaoaraya <joaoaraya7@gmail.com>
2019-05-13 04:41:43 +0900

    Correção dos produtos não cadastrados

--- www/js/produtos.js
+++ www/js/produtos.js
@@ -2,20 +2,6 @@
 
 function scanBarcode() {
   window.plugins.barcodeScanner.scan( function(result) {
-    if (result.text == "") // Produto não econtrado
-    {
-      document.getElementById("scan-titulo").textContent="PRODUTO NÃO ENCONTRADO";
-      document.getElementById('produto-img-1').src="img/notfound.png"
-      document.getElementById("produto-txt-1").textContent="";
-      document.getElementById("produto-txt-2").textContent="";
-      document.getElementById("produto-txt-3").textContent="";
-      document.getElementById("produto-txt-4").textContent="";
-      document.getElementById("produto-txt-5").textContent="";
-      document.getElementById("produto-txt-6").textContent="";
-      document.getElementById("produto-txt-7").textContent="";
-      document.getElementById("produto-txt-8").textContent="";
-      document.getElementById("produto-txt-9").textContent="";
-    }
     if (result.text == "280720550") // Produto 1
     {
       document.getElementById("scan-titulo").textContent="PRODUTO ENCONTRADO";
@@ -30,7 +16,7 @@ function scanBarcode() {
       document.getElementById("produto-txt-8").textContent="Tela: 7.9";
       document.getElementById("produto-txt-9").textContent="Camera: 8MP";
     }
-    if (result.text == "989895555") // Produto 2
+    else if (result.text == "989895555") // Produto 2
     {
       document.getElementById("scan-titulo").textContent="PRODUTO ENCONTRADO";
       document.getElementById('produto-img-1').src="img/produto2.png"
@@ -44,7 +30,7 @@ function scanBarcode() {
       document.getElementById("produto-txt-8").textContent="Tela: 6";
       document.getElementById("produto-txt-9").textContent="Camera: 16MP";
     }
-    if (result.text == "85236987") // Produto 3
+    else if (result.text == "85236987") // Produto 3
     {
       document.getElementById("scan-titulo").textContent="PRODUTO ENCONTRADO";
       document.getElementById('produto-img-1').src="img/produto3.png"
@@ -58,7 +44,7 @@ function scanBarcode() {
       document.getElementById("produto-txt-8").textContent="Tela: 6";
       document.getElementById("produto-txt-9").textContent="Camera: 13MP";
     }
-    if (result.text == "85369877444") // Produto 4
+    else if (result.text == "85369877444") // Produto 4
     {
       document.getElementById("scan-titulo").textContent="PRODUTO ENCONTRADO";
       document.getElementById('produto-img-1').src="img/produto4.png"
@@ -71,6 +57,20 @@ function scanBarcode() {
       document.getElementById("produto-txt-7").textContent="Memoria Ram: 4GB";
       document.getElementById("produto-txt-8").textContent="Tela: 6";
       document.getElementById("produto-txt-9").textContent="Camera: 16MP";
+    }
+    else // Produto não econtrado
+    {
+      document.getElementById("scan-titulo").textContent="PRODUTO NÃO ENCONTRADO";
+      document.getElementById('produto-img-1').src="img/notfound.png"
+      document.getElementById("produto-txt-1").textContent="";
+      document.getElementById("produto-txt-2").textContent="";
+      document.getElementById("produto-txt-3").textContent="";
+      document.getElementById("produto-txt-4").textContent="";
+      document.getElementById("produto-txt-5").textContent="";
+      document.getElementById("produto-txt-6").textContent="";
+      document.getElementById("produto-txt-7").textContent="";
+      document.getElementById("produto-txt-8").textContent="";
+      document.getElementById("produto-txt-9").textContent="";
     }
       }, function(error) {
         alert("Erro ao escanear: " + error);
